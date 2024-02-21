e06e1335f2fc02a34abb8b4a7c8bd9a4922c93c9
Denis Komandyr <sabsdaid@gmail.com>
2024-02-21 17:00:08 +0200

    Init project

---
+++ db/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Numeric, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    price: Mapped[Numeric] = mapped_column(Numeric(10, 2))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
