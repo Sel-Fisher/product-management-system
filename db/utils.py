44c28f593a5895b5ec8d548045b194c8065c57c9
Denis Komandyr <sabsdaid@gmail.com>
2024-02-22 23:21:14 +0200

    Endpoints for manage products

--- db/utils.py
+++ db/utils.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from db.database import SessionLocal
@@ -9,3 +12,6 @@ def get_db() -> Session:
         yield db
     finally:
         db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
