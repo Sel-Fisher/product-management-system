4097814c6da0ee551eed87451d5fd33243fcda23
Denis Komandyr <sabsdaid@gmail.com>
2024-02-22 23:19:05 +0200

    Create schemas

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from decimal import Decimal
+from pydantic import BaseModel, ConfigDict
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: Decimal
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
+    created_at: datetime
+    model_config = ConfigDict(
+        from_attributes=True
+    )
